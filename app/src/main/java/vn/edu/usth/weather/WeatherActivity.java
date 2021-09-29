05ac83c64c257bbb3ff224e992a72f8adb4f9d67
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-09-29 22:26:38 +0700

    update weather activity

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,12 +3,46 @@ package vn.edu.usth.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class WeatherActivity extends AppCompatActivity {
-
+    private static final String TAG = "WeatherActivity";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        Log.i(TAG, "onCreate()");
     }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.i(TAG, "onStart()");
+    };
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.i(TAG, "onRestart()");
+    };
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i(TAG, "onResume()");
+    };
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i(TAG, "onPause()");
+    };
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    };
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
+    };
+
 }
