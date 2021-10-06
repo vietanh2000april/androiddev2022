a568f57cc9d8e7c5554d1d4cf64b9b6357711431
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-10-06 16:24:45 +0700

    apply dark mode background color

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -61,7 +61,8 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_forecast, container, false);
-        v.setBackgroundColor(Color.GREEN);
+        int grey_dark_mode = Color.parseColor("#121212");
+        v.setBackgroundColor(grey_dark_mode);
         return v;
     }
 }
