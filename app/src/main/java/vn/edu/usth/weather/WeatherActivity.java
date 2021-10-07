dbc3ae481a43d379db7b523a26d9e1bda83ae9a6
levietanh_bi9035 <anhlv.bi9035@st.usth.edu.vn>
2021-10-08 00:33:17 +0700

    (use code = failed) add weather fragment above forecast fragment

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -15,8 +15,11 @@ public class WeatherActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+
+        ForecastFragment fragment_forecast = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_weather, fragment_forecast).commit();
+        WeatherFragment fragment_weather = new WeatherFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.activity_weather, fragment_weather).commit();
         Log.i(TAG, "onCreate()");
     }
     @Override
